b16881898bd9c698d19ae9d593dd90f7e9bf6e2b
Tanner Stokes <tanner@tannr.com>
2017-03-21 22:29:54 -0400

    Improvements, external config

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # health-data
 Health Data
 
+Python 3
+
+pip install -r requirements.txt
+
 To Do: Query for the latest record before doing anything, to cut down on time
 See if values that end up as None are ignored in the DB or need to be filtered beforehand
 
-
-Problem records:
-# for record in records[255000:260000]:
--- health_to_influx.py
+++ health_to_influx.py
@@ -1,81 +1,128 @@
+import sys
+import yaml
 import argparse
 import xml.etree.ElementTree as ET
-from datetime import datetime, timezone
 from influxdb import InfluxDBClient
+from datetime import datetime, timezone
 
 
-def upload(host, data=[], database='health'):
+def upload(config, data=[]):
     if len(data):
-        client = InfluxDBClient(host=host, database=database, port=8086)
-        client.create_database('health')
-        client.write_points(points=data, batch_size=1000)
+        client = InfluxDBClient(**config['influxdb']['client'])
+        client.create_database(config['influxdb']['client']['database'])
+        client.write_points(points=data, **config['influxdb']['write_points'])
 
-def parse(export):
-    with open(export) as f:
+def parse(file):
+    with open(file) as f:
         tree = ET.parse(f)
 
     formattedData = []
+    # get all records
     records = tree.findall('Record')
-
     # we only want records with numeric values
-    records = list(filter(lambda x: str(x.get('value')).isdigit(), records))
+    points = [x for x in records[0:10] if createPoint(x)]
 
-    for record in records:
-        attr = record.attrib
+    return points
 
-        unit = attr['unit']
-        value = float(attr['value'])
-        date = attr['endDate']
-        source = attr['sourceName']
-        measurement = attr['type']
+    # for record in records:
+    #     attr = record.attrib
 
-        # chop off prefix if detected
-        if measurement[0:24] == 'HKQuantityTypeIdentifier':
-            measurement = measurement[24:]
+    #     # unit = attr['unit']
 
-        # parse the time string
-        parsedTime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S %z')
-        # save as correct format in UTC timezone
-        time = parsedTime.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    #     value = attr['value']
+    #     date = attr['endDate']
+    #     measurement = attr['type']
+    #     # source = attr['sourceName']
+        
+    #     # chop off prefix if detected
+    #     if measurement[0:24] == 'HKQuantityTypeIdentifier':
+    #         measurement = measurement[24:]
 
-        recordDict = {
-            'measurement': measurement,
-            'tags': {},
-            'time': time,
-            'fields': {
-                'value': value
-            }
+    #     # parse the time string
+    #     parsedTime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S %z')
+    #     # save as correct format in UTC timezone
+    #     time = parsedTime.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    #     recordDict = {
+    #         'measurement': measurement,
+    #         'tags': {},
+    #         'time': time,
+    #         'fields': {
+    #             'value': value
+    #         }
+    #     }
+
+    #     if unit in attr:
+    #         recordDict['tags']['unit'] = attr['unit']
+    #     if source in attr:
+    #         recordDict['tags']['source'] = attr['source']
+
+    #     formattedData.append(recordDict)
+
+    # return formattedData
+
+def createPoint(record):
+    attr = record.attrib
+
+    date = attr.get('date')
+    unit = attr.get('unit')
+    value = attr.get('value')
+    source = attr.get('source')
+    measurement = attr.get('type')
+
+    if date is None or value is None or measurement is None:
+        return False
+
+    # chop off prefix if detected
+    if measurement[0:24] == 'HKQuantityTypeIdentifier':
+        measurement = measurement[24:]
+
+    # parse the time string
+    parsedTime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S %z')
+    # save as correct format in UTC timezone
+    time = parsedTime.astimezone(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    recordDict = {
+        'measurement': measurement,
+        'tags': {},
+        'time': time,
+        'fields': {
+            'value': value
         }
+    }
 
-        if unit is not None:
-            recordDict['tags']['unit'] = unit
-        if source is not None:
-            recordDict['tags']['source'] = source
+    if not unit:
+        recordDict['tags']['unit'] = unit
+    if not source:
+        recordDict['tags']['source'] = source
 
-        formattedData.append(recordDict)
+    return recordDict
 
-    # print(formattedData)
-    return formattedData
+def loadConfig(configFile):
+    with open(configFile) as f:
+        config = yaml.safe_load(f)
 
+    return config
 
-def main(host, export, database):
+def main(configFile, exportFile):
     try:
-        data = parse(export)
-        upload(host, data, database)
+        config = loadConfig(configFile)
+        data = parse(exportFile)
+        print(len(data))
+        upload(config, data)
 
         print('Total upload success!')
     except Exception as e:
         print('Failure!')
-        print(str(e))
+        e = sys.exc_info()[0]
+        print(e)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Uploads HealthKit data to InfluxDB')
-    parser.add_argument('dbhost', help='InfluxDB host')
-    # TODO - make sure this is optional
-    parser.add_argument('database', default='health', help="InfluxDB database")
-    parser.add_argument('file', help='Health data export file')
+    parser.add_argument('config_file', help='InfluxDB config file location', default='./config.yml')
+    parser.add_argument('export_file', help='Apple Health data export file')
 
     args = parser.parse_args()
 
-    main(args.dbhost, args.file, args.database)
+    main(args.config_file, args.export_file)
