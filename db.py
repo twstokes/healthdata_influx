c29f64717b4c8ca2f3dfb2c6517e3daddeeaa024
Tanner Stokes <tanner@tannr.com>
2017-03-30 21:17:29 -0400

    More PEP 8 friendly

--- db.py
+++ db.py
@@ -1,21 +1,19 @@
+"""Loads a configuration and imports data points into InfluxDB"""
 import yaml
 from influxdb import InfluxDBClient
 
-"""
-Uploads data points to InfluxDB
-"""
-def upload(configPath, dataPoints=[]):
-    config = _loadConfig(configPath)
+def upload(configpath, datapoints=None):
+    """Uploads data points to InfluxDB"""
+    config = _loadconfig(configpath)
 
-    if len(dataPoints):
+    if datapoints is not None and len(datapoints):
         client = InfluxDBClient(**config['influxdb']['client'])
         client.create_database(config['influxdb']['client']['database'])
-        client.write_points(points=dataPoints, **config['influxdb']['write_points'])
+        client.write_points(points=datapoints, **config['influxdb']['write_points'])
 
-"""
-Loads config for this script
-"""
-def _loadConfig(configPath):
-    with open(configPath) as f:
-        config = yaml.safe_load(f)
+
+def _loadconfig(configpath):
+    """Loads config for this script"""
+    with open(configpath) as file:
+        config = yaml.safe_load(file)
     return config
