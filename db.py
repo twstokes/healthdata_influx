53222d9c4ceb145427ccee28867a7fb73a2e576b
Tanner Stokes <tanner@tannr.com>
2017-03-30 21:20:59 -0400

    Even better!

--- db.py
+++ db.py
@@ -2,18 +2,18 @@
 import yaml
 from influxdb import InfluxDBClient
 
-def upload(configpath, datapoints=None):
+def upload(config_path, data_points=None):
     """Uploads data points to InfluxDB"""
-    config = _loadconfig(configpath)
+    config = _load_config(config_path)
 
-    if datapoints is not None and len(datapoints):
+    if data_points is not None and len(data_points):
         client = InfluxDBClient(**config['influxdb']['client'])
         client.create_database(config['influxdb']['client']['database'])
-        client.write_points(points=datapoints, **config['influxdb']['write_points'])
+        client.write_points(points=data_points, **config['influxdb']['write_points'])
 
 
-def _loadconfig(configpath):
+def _load_config(config_path):
     """Loads config for this script"""
-    with open(configpath) as file:
+    with open(config_path) as file:
         config = yaml.safe_load(file)
     return config
